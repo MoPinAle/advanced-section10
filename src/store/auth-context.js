6c04bf73fc7ab244e8ed92dabee6d9e1c0d4957f
Zucas <PaivaTech@hotmail.com>
2021-08-04 18:38:52 +0100

    I created the context ad applied the reducer.

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogout: () => {},
-  onLogin: (email, password) => {},
+  onLogin: (email, password) => {}
 });
 
 export const AuthContextProvider = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storageUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if(storageUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
   const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
